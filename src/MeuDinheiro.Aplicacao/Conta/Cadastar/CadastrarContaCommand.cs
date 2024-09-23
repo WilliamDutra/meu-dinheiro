fe9fc5900a23344952095963b8f8011ee92a817d
William Dutra <will.dutra26@gmail.com>
2024-09-23 12:58:21 -0300

    feat(conta): inclusão do cadastra de conta

---
+++ src/MeuDinheiro.Aplicacao/Conta/Cadastar/CadastrarContaCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MeuDinheiro.Shared;
+
+namespace MeuDinheiro.Aplicacao.Conta.Cadastar
+{
+    public class CadastrarContaCommand : Command
+    {
+        public string Nome { get; set; }
+
+        public override bool Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MeuDinheiro.Aplicacao/Conta/Cadastar/CadastrarContaCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MeuDinheiro.Shared;
+using MeuDinheiro.Dominio.Repositorios;
+
+namespace MeuDinheiro.Aplicacao.Conta.Cadastar
+{
+    public class CadastrarContaCommandHandler : ICommandHandler<CadastrarContaCommand, CommandResult>
+    {
+        private IContaRepositorio _contaRepositorio;
+
+        public CadastrarContaCommandHandler(IContaRepositorio contaRepositorio)
+        {
+            _contaRepositorio = contaRepositorio;
+        }
+
+        public CommandResult Handle(CadastrarContaCommand command)
+        {
+
+            command.Validate();
+
+            if (!command.IsValid)
+                return new CommandResult(true, string.Join(",", command.Errors.ToArray()));
+
+            var conta = MeuDinheiro.Dominio.Conta.Criar(command.Nome);
+
+            return new CommandResult(true, "Conta criada com sucesso!");
+        }
+    }
+}
