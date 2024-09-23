3ec0d8b81b3bd748e550df8d271d03b60021814a
William Dutra <will.dutra26@gmail.com>
2024-09-23 12:32:22 -0300

    feat(despesa): inclusão do lançamento de despesa

---
+++ src/MeuDinheiro.Aplicacao/Despesa/LancarDespesa/LancarDespesaCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MeuDinheiro.Shared;
+using MeuDinheiro.Dominio;
+
+namespace MeuDinheiro.Aplicacao.Despesa.LancarDespesa
+{
+    public class LancarDespesaCommand : Command
+    {
+        public string Nome { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public ETipoDespesa Tipo { get; set; }
+
+        public override bool Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MeuDinheiro.Aplicacao/Despesa/LancarDespesa/LancarDespesaCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MeuDinheiro.Shared;
+using MeuDinheiro.Dominio.Repositorios;
+
+namespace MeuDinheiro.Aplicacao.Despesa.LancarDespesa
+{
+    public class LancarDespesaCommandHandler : ICommandHandler<LancarDespesaCommand, CommandResult>
+    {
+        private IContaRepositorio _contaRepositorio;
+
+        public LancarDespesaCommandHandler(IContaRepositorio contaRepositorio)
+        {
+            _contaRepositorio = contaRepositorio;
+        }
+
+        public CommandResult Handle(LancarDespesaCommand command)
+        {
+            command.Validate();
+
+            if (!command.IsValid)
+                return new CommandResult(false, string.Join(",", command.Errors.ToArray()));
+
+            var lancamento = MeuDinheiro.Dominio.Despesa.Criar(command.Nome, command.Valor, command.Tipo, DateTime.Now);
+            int lancamentoId = _contaRepositorio.Salvar(lancamento);
+
+            return new CommandResult(true, "Lançamento incluído com sucesso!", lancamentoId);
+        }
+    }
+}
